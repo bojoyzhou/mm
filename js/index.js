d4ee3c1d44030f9b8369808b797fb571b2552935
bojoyzhou <453398480@qq.com>
2016-12-16 18:25:16 +0800

    随机颜色

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 const PW = 50,
     PH = 20
+const colors = ["rgb(244, 67, 54)","rgb(239, 83, 80)","rgb(244, 67, 54)","rgb(229, 57, 53)","rgb(211, 47, 47)","rgb(198, 40, 40)","rgb(183, 28, 28)","rgb(255, 82, 82)","rgb(255, 23, 68)","rgb(213, 0, 0)","rgb(233, 30, 99)","rgb(240, 98, 146)","rgb(236, 64, 122)","rgb(233, 30, 99)","rgb(216, 27, 96)","rgb(194, 24, 91)","rgb(173, 20, 87)","rgb(136, 14, 79)","rgb(255, 64, 129)","rgb(245, 0, 87)","rgb(197, 17, 98)","rgb(156, 39, 176)","rgb(186, 104, 200)","rgb(171, 71, 188)","rgb(156, 39, 176)","rgb(142, 36, 170)","rgb(123, 31, 162)","rgb(106, 27, 154)","rgb(74, 20, 140)","rgb(224, 64, 251)","rgb(213, 0, 249)","rgb(170, 0, 255)","rgb(103, 58, 183)","rgb(149, 117, 205)","rgb(126, 87, 194)","rgb(103, 58, 183)","rgb(94, 53, 177)","rgb(81, 45, 168)","rgb(69, 39, 160)","rgb(49, 27, 146)","rgb(124, 77, 255)","rgb(101, 31, 255)","rgb(98, 0, 234)","rgb(63, 81, 181)","rgb(121, 134, 203)","rgb(92, 107, 192)","rgb(63, 81, 181)","rgb(57, 73, 171)","rgb(48, 63, 159)","rgb(40, 53, 147)","rgb(26, 35, 126)","rgb(83, 109, 254)","rgb(61, 90, 254)","rgb(48, 79, 254)","rgb(33, 150, 243)","rgb(33, 150, 243)","rgb(30, 136, 229)","rgb(25, 118, 210)","rgb(21, 101, 192)","rgb(13, 71, 161)","rgb(68, 138, 255)","rgb(41, 121, 255)","rgb(41, 98, 255)","rgb(3, 169, 244)","rgb(3, 155, 229)","rgb(2, 136, 209)","rgb(2, 119, 189)","rgb(1, 87, 155)","rgb(0, 145, 234)","rgb(0, 188, 212)","rgb(0, 151, 167)","rgb(0, 131, 143)","rgb(0, 96, 100)","rgb(0, 150, 136)","rgb(0, 150, 136)","rgb(0, 137, 123)","rgb(0, 121, 107)","rgb(0, 105, 92)","rgb(0, 77, 64)","rgb(76, 175, 80)","rgb(67, 160, 71)","rgb(56, 142, 60)","rgb(46, 125, 50)","rgb(27, 94, 32)","rgb(139, 195, 74)","rgb(104, 159, 56)","rgb(85, 139, 47)","rgb(51, 105, 30)","rgb(205, 220, 57)","rgb(130, 119, 23)","rgb(255, 235, 59)","rgb(255, 193, 7)","rgb(255, 152, 0)","rgb(239, 108, 0)","rgb(230, 81, 0)","rgb(255, 87, 34)","rgb(255, 87, 34)","rgb(244, 81, 30)","rgb(230, 74, 25)","rgb(216, 67, 21)","rgb(191, 54, 12)","rgb(255, 61, 0)","rgb(221, 44, 0)","rgb(121, 85, 72)","rgb(161, 136, 127)","rgb(141, 110, 99)","rgb(121, 85, 72)","rgb(109, 76, 65)","rgb(93, 64, 55)","rgb(78, 52, 46)","rgb(62, 39, 35)","rgb(158, 158, 158)","rgb(117, 117, 117)","rgb(97, 97, 97)","rgb(66, 66, 66)","rgb(33, 33, 33)","rgb(96, 125, 139)","rgb(120, 144, 156)","rgb(96, 125, 139)","rgb(84, 110, 122)","rgb(69, 90, 100)","rgb(55, 71, 79)","rgb(38, 50, 56)","rgb(0, 0, 0)"]
+let cursor = 0
+let flag = []
 
+function randomColor() {
+    cursor %= colors.length
+    if (flag[cursor]) {
+        return randomColor()
+    }
+    const color =  colors[cursor]
+    cursor += parseInt(Math.random() * 100) % 20
+    return color
+}
 class Node {
     static id = 1;
     constructor() {
@@ -9,6 +21,7 @@ class Node {
         this.text = 'default Value'
         this.children = []
         this.ischanged = true
+        this.color = randomColor()
         this.id = Node.id++
     }
     setPos(left, top) {
@@ -57,10 +70,8 @@ class Node {
         this.ischanged = true
     }
     serialize() {
-        const { left, top, text, children } = this
+        const { text, children } = this
         return {
-            left,
-            top,
             text,
             children: children.map(child => child.serialize())
         }
@@ -81,6 +92,7 @@ class Edge {
         this.a = a
         this.b = b
         this.ischanged = true
+        this.color = a.color
         this.id = Edge.id++
     }
     setD(d) {
@@ -114,6 +126,17 @@ class Data extends Node {
     }
     serialize() {
         return this.data.map(node => node.serialize())
+    }
+    parseNode(desc, parent) {
+        let node = this.addNode({ parent, text: desc.text })
+        desc.children.forEach(d => {
+            this.parseNode(d, node)
+        })
+    }
+    parse(nodes) {
+        nodes.forEach(node => {
+            this.parseNode(node)
+        })
     }
 }
 
@@ -154,7 +177,6 @@ class Calculator {
     }
     calNode() {
         this.data.data.forEach(rootNode => {
-            debugger
             const offset = this.balance(rootNode)
             const { t_width, t_height, t_left, t_right, t_top, t_bottom } = offset
             rootNode.t_width = t_width
@@ -293,7 +315,8 @@ class View {
             return this.edgeMap[id]
         } else {
             let div = this.createPath({
-                stroke: '#F44336',
+                stroke: edge.color,
+                strokeWidth: '3px',
                 fill: 'transparent',
                 filter: 'url(#blurMe)'
             })
@@ -341,13 +364,9 @@ class Renderer {
 
 
 var d = new Data()
-var n1 = d.addNode({})
-// n1.setPos(500, 500)
-var n1s = [d.addNode({ parent: n1, text: 'node 1' }), d.addNode({ parent: n1, text: 'node 2' }), d.addNode({ parent: n1, text: 'node 3' }), d.addNode({ parent: n1, text: 'node 4' })]
-var n2 = n1s[0]
-var n2s = [d.addNode({ parent: n2, text: 'node 1.1' }), d.addNode({ parent: n2, text: 'node 1.2' }), d.addNode({ parent: n2, text: 'node 1.3' }), d.addNode({ parent: n2, text: 'node 1.4' })]
-var n3 = n1s[1]
-var n3s = [d.addNode({ parent: n3, text: 'node 2.1' }), d.addNode({ parent: n3, text: 'node 2.2' }), d.addNode({ parent: n3, text: 'node 2.3' }), d.addNode({ parent: n3, text: 'node 2.4' })]
+var data = [{ "text": "default value", "children": [{ "text": "node 1", "children": [{ "text": "node 1.1", "children": [] }, { "text": "node 1.2", "children": [] }, { "text": "node 1.3", "children": [] }, { "text": "node 1.4", "children": [] }] }, { "text": "node 2", "children": [{ "text": "node 2.1", "children": [] }, { "text": "node 2.2", "children": [] }, { "text": "node 2.3", "children": [] }, { "text": "node 2.4", "children": [] }] }, { "text": "node 3", "children": [] }, { "text": "node 4", "children": [] }] }]
+
+d.parse(data)
 
 console.log(d.serialize())
 
