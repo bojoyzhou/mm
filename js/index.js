c9e0fcb467051fd37fe2a8f5538e658b44055111
bojoyzhou <453398480@qq.com>
2016-12-21 12:01:54 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import data from './data'
+import makeLines from '../lib/line'
 const PW = 50,
     PH = 20
 const colors = ["rgb(244, 67, 54)", "rgb(239, 83, 80)", "rgb(244, 67, 54)", "rgb(229, 57, 53)", "rgb(211, 47, 47)", "rgb(198, 40, 40)", "rgb(183, 28, 28)", "rgb(255, 82, 82)", "rgb(255, 23, 68)", "rgb(213, 0, 0)", "rgb(233, 30, 99)", "rgb(240, 98, 146)", "rgb(236, 64, 122)", "rgb(233, 30, 99)", "rgb(216, 27, 96)", "rgb(194, 24, 91)", "rgb(173, 20, 87)", "rgb(136, 14, 79)", "rgb(255, 64, 129)", "rgb(245, 0, 87)", "rgb(197, 17, 98)", "rgb(156, 39, 176)", "rgb(186, 104, 200)", "rgb(171, 71, 188)", "rgb(156, 39, 176)", "rgb(142, 36, 170)", "rgb(123, 31, 162)", "rgb(106, 27, 154)", "rgb(74, 20, 140)", "rgb(224, 64, 251)", "rgb(213, 0, 249)", "rgb(170, 0, 255)", "rgb(103, 58, 183)", "rgb(149, 117, 205)", "rgb(126, 87, 194)", "rgb(103, 58, 183)", "rgb(94, 53, 177)", "rgb(81, 45, 168)", "rgb(69, 39, 160)", "rgb(49, 27, 146)", "rgb(124, 77, 255)", "rgb(101, 31, 255)", "rgb(98, 0, 234)", "rgb(63, 81, 181)", "rgb(121, 134, 203)", "rgb(92, 107, 192)", "rgb(63, 81, 181)", "rgb(57, 73, 171)", "rgb(48, 63, 159)", "rgb(40, 53, 147)", "rgb(26, 35, 126)", "rgb(83, 109, 254)", "rgb(61, 90, 254)", "rgb(48, 79, 254)", "rgb(33, 150, 243)", "rgb(33, 150, 243)", "rgb(30, 136, 229)", "rgb(25, 118, 210)", "rgb(21, 101, 192)", "rgb(13, 71, 161)", "rgb(68, 138, 255)", "rgb(41, 121, 255)", "rgb(41, 98, 255)", "rgb(3, 169, 244)", "rgb(3, 155, 229)", "rgb(2, 136, 209)", "rgb(2, 119, 189)", "rgb(1, 87, 155)", "rgb(0, 145, 234)", "rgb(0, 188, 212)", "rgb(0, 151, 167)", "rgb(0, 131, 143)", "rgb(0, 96, 100)", "rgb(0, 150, 136)", "rgb(0, 150, 136)", "rgb(0, 137, 123)", "rgb(0, 121, 107)", "rgb(0, 105, 92)", "rgb(0, 77, 64)", "rgb(76, 175, 80)", "rgb(67, 160, 71)", "rgb(56, 142, 60)", "rgb(46, 125, 50)", "rgb(27, 94, 32)", "rgb(139, 195, 74)", "rgb(104, 159, 56)", "rgb(85, 139, 47)", "rgb(51, 105, 30)", "rgb(205, 220, 57)", "rgb(130, 119, 23)", "rgb(255, 235, 59)", "rgb(255, 193, 7)", "rgb(255, 152, 0)", "rgb(239, 108, 0)", "rgb(230, 81, 0)", "rgb(255, 87, 34)", "rgb(255, 87, 34)", "rgb(244, 81, 30)", "rgb(230, 74, 25)", "rgb(216, 67, 21)", "rgb(191, 54, 12)", "rgb(255, 61, 0)", "rgb(221, 44, 0)", "rgb(121, 85, 72)", "rgb(161, 136, 127)", "rgb(141, 110, 99)", "rgb(121, 85, 72)", "rgb(109, 76, 65)", "rgb(93, 64, 55)", "rgb(78, 52, 46)", "rgb(62, 39, 35)", "rgb(158, 158, 158)", "rgb(117, 117, 117)", "rgb(97, 97, 97)", "rgb(66, 66, 66)", "rgb(33, 33, 33)", "rgb(96, 125, 139)", "rgb(120, 144, 156)", "rgb(96, 125, 139)", "rgb(84, 110, 122)", "rgb(69, 90, 100)", "rgb(55, 71, 79)", "rgb(38, 50, 56)", "rgb(0, 0, 0)"]
@@ -73,14 +74,16 @@ class Node {
                 node.moveTo(ftl, ftb + PH - node.height / 2 + node.t_height / 2)
                 ftb = node.t_bottom
             }
+            this.width = width
+            this.height = height
+
             //最后一个节点的底边
             const ltb = node.t_bottom
-                //节点向右展开，当前节点左边等于子节点的左边减去间隔减去当前节点宽度
+
+            //节点向右展开，当前节点左边等于子节点的左边减去间隔减去当前节点宽度
             let nleft = node.left - PW - this.width,
                 ntop = ftt + (ltb - ftt) / 2 - this.height / 2
 
-            this.width = width
-            this.height = height
 
             this.setPos(nleft, ntop)
         }
@@ -100,16 +103,11 @@ class Node {
             n.root = this.root
             n.data = this.data
             n.color = this.color
+
+            n.edge = new Edge(this, n)
         }
         n.width = Math.max(10 - n.level, 2)
         n.parent = this
-        n.parentEdge = new Edge({
-            p0: n.parent.getEdgeC(),
-            p1: n.getEdgeP(),
-            color: n.color,
-            startWidth: n.parent.width,
-            endWidth: n.width
-        })
         this.children.push(n)
         return n
     }
@@ -149,9 +147,6 @@ class Node {
             this.t_bottom = this.bottom
             this.t_left = this.left
         }
-        if (this.parentEdge) {
-            this.parentEdge.moveTo(this.parent.getEdgeP(), this.getEdgeC())
-        }
     }
     resize() {
         this.right = this.left + this.width
@@ -181,15 +176,88 @@ class Node {
     }
 }
 class Edge {
-    constructor(p0, p1) {
-
+    constructor({ parent, child }) {
+        this.parent = parent
+        this.child = child
+    }
+    setCanvas(canvas) {
+        this.canvas = canvas
     }
     moveTo(p0, p1) {
 
     }
+    renderArc() {
+        let p0 = this.parent.getEdgeP(),
+            p3 = this.getEdgeC(),
+            color = this.color,
+            startWidth = this.parent.width,
+            endWidth = this.width,
+            p1, p2
+        if (p0[0] < p3[0]) {
+            p1 = [p0[0] + 100, p0[1]]
+            p2 = [p3[0] - 100, p3[1]]
+        } else {
+            p1 = [p0[0] - 100, p0[1]]
+            p2 = [p3[0] + 100, p3[1]]
+        }
+        this.canvas.drawBezier({
+            p0,
+            p1,
+            p2,
+            p3,
+            color,
+            startWidth,
+            endWidth
+        })
+    }
+    renderLine() {
+        let p0 = this.getEdgeP(),
+            p1 = this.getEdgeC(),
+            color = this.color,
+            width = this.width
+        this.canvas.drawLine({
+            p0,
+            p1,
+            color,
+            width
+        })
+    }
+    render() {
+        this.p0 = p0
+        this.p1 = p1
+        this.color = color
+        this.startWidth = startWidth
+        this.endWidth = endWidth
+        this.width = width
+        this.renderArc()
+        this.renderLine()
+    }
 }
 class Canvas {
+    constructor() {
+        this.canvas = document.getElementById('canvas')
+        const N = 2
+        canvas.width = document.body.offsetWidth * N
+        canvas.height = document.body.offsetHeight * N
+        this.ctx = canvas.getContext('2d')
+        this.shapes = []
+        const { clear, drawLine, drawBezier } = makeLines(thi.ctx)
+        this.clear = clear
+        this.drawLine = drawLine
+        this.drawBezier = drawBezier
 
+        this.render = this.render.bind(this)
+        this.render()
+    }
+    addChild(shape) {
+        this.shapes.push(shapes)
+        shape.setCanvas(this)
+    }
+    render() {
+        this.shapes.render()
+        this.clear()
+        requestAnimationFrame(this.render)
+    }
 }
 class Data extends Node {
     constructor(data) {
@@ -218,6 +286,17 @@ class Data extends Node {
                 child.render()
             })
         }
+    }
+    getNodeEdges(node){
+        return [...node.children.map(child => (this.getNodeEdges(child))), node.edge]
+    }
+    getEdges(){
+        var edges = []
+        this.children.forEach(child => {
+            if(child)
+            edges = [...edges, ...this.getNodeEdges(child)]
+        })
+        return edges
     }
 }
 class Render {
