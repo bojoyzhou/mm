7e7950930db635594ae9f26e173f59e17a5d456d
bojoyzhou <453398480@qq.com>
2016-12-25 23:08:32 +0800

    update to canvas

--- js/data.js
+++ js/data.js
@@ -1,44 +1,47 @@
 var data = {
     "text": "双击开始",
     "children": [{
-        "text": "node 1",
+        "text": "Big Text jylZnode 1",
         "children": [{
-            "text": "node 1.1",
+            "text": "Big Text jylZnode 1.1",
             "children": []
         }, {
-            "text": "node 1.2",
+            "text": "Big Text jylZnode 1.2",
             "children": []
         }, {
-            "text": "node 1.3",
+            "text": "Big Text jylZnode 1.3",
             "children": []
         }, {
-            "text": "node 1.4",
+            "text": "Big Text jylZnode 1.4",
             "children": []
         }]
     }, {
-        "text": "node 2",
+        "text": "Big Text jylZnode 2",
         "children": [{
-            "text": "node 2.1",
+            "text": "Big Text jylZnode 2.1",
             "children": []
         }, {
-            "text": "node 2.2",
+            "text": "Big Text jylZnode 2.2",
             "children": []
         }, {
-            "text": "node 2.3",
+            "text": "Big Text jylZnode 2.3",
             "children": []
         }, {
-            "text": "node 2.4",
+            "text": "Big Text jylZnode 2.4",
             "children": []
         }]
     }, {
-        "text": "node 3",
+        "text": "Big Text jylZnode 3",
         "children": [{
-            "text": "node 3.1",
+            "text": "Big Text jylZnode 3.1",
             "children": []
         }]
     }, {
-        "text": "node 4",
-        "children": []
+        "text": "Big Text jylZnode 4",
+        "children": [{
+            "text": "Big Text jylZnode 3.1",
+            "children": []
+        }]
     }]
 }
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import EventEmitter from 'events'
 import data from './data'
-const PW = 70,
+const PW = 100,
     PH = 20
 const colors = ["rgb(244, 67, 54)", "rgb(239, 83, 80)", "rgb(244, 67, 54)", "rgb(229, 57, 53)", "rgb(211, 47, 47)", "rgb(198, 40, 40)", "rgb(183, 28, 28)", "rgb(255, 82, 82)", "rgb(255, 23, 68)", "rgb(213, 0, 0)", "rgb(233, 30, 99)", "rgb(240, 98, 146)", "rgb(236, 64, 122)", "rgb(233, 30, 99)", "rgb(216, 27, 96)", "rgb(194, 24, 91)", "rgb(173, 20, 87)", "rgb(136, 14, 79)", "rgb(255, 64, 129)", "rgb(245, 0, 87)", "rgb(197, 17, 98)", "rgb(156, 39, 176)", "rgb(186, 104, 200)", "rgb(171, 71, 188)", "rgb(156, 39, 176)", "rgb(142, 36, 170)", "rgb(123, 31, 162)", "rgb(106, 27, 154)", "rgb(74, 20, 140)", "rgb(224, 64, 251)", "rgb(213, 0, 249)", "rgb(170, 0, 255)", "rgb(103, 58, 183)", "rgb(149, 117, 205)", "rgb(126, 87, 194)", "rgb(103, 58, 183)", "rgb(94, 53, 177)", "rgb(81, 45, 168)", "rgb(69, 39, 160)", "rgb(49, 27, 146)", "rgb(124, 77, 255)", "rgb(101, 31, 255)", "rgb(98, 0, 234)", "rgb(63, 81, 181)", "rgb(121, 134, 203)", "rgb(92, 107, 192)", "rgb(63, 81, 181)", "rgb(57, 73, 171)", "rgb(48, 63, 159)", "rgb(40, 53, 147)", "rgb(26, 35, 126)", "rgb(83, 109, 254)", "rgb(61, 90, 254)", "rgb(48, 79, 254)", "rgb(33, 150, 243)", "rgb(33, 150, 243)", "rgb(30, 136, 229)", "rgb(25, 118, 210)", "rgb(21, 101, 192)", "rgb(13, 71, 161)", "rgb(68, 138, 255)", "rgb(41, 121, 255)", "rgb(41, 98, 255)", "rgb(3, 169, 244)", "rgb(3, 155, 229)", "rgb(2, 136, 209)", "rgb(2, 119, 189)", "rgb(1, 87, 155)", "rgb(0, 145, 234)", "rgb(0, 188, 212)", "rgb(0, 151, 167)", "rgb(0, 131, 143)", "rgb(0, 96, 100)", "rgb(0, 150, 136)", "rgb(0, 150, 136)", "rgb(0, 137, 123)", "rgb(0, 121, 107)", "rgb(0, 105, 92)", "rgb(0, 77, 64)", "rgb(76, 175, 80)", "rgb(67, 160, 71)", "rgb(56, 142, 60)", "rgb(46, 125, 50)", "rgb(27, 94, 32)", "rgb(139, 195, 74)", "rgb(104, 159, 56)", "rgb(85, 139, 47)", "rgb(51, 105, 30)", "rgb(205, 220, 57)", "rgb(130, 119, 23)", "rgb(255, 235, 59)", "rgb(255, 193, 7)", "rgb(255, 152, 0)", "rgb(239, 108, 0)", "rgb(230, 81, 0)", "rgb(255, 87, 34)", "rgb(255, 87, 34)", "rgb(244, 81, 30)", "rgb(230, 74, 25)", "rgb(216, 67, 21)", "rgb(191, 54, 12)", "rgb(255, 61, 0)", "rgb(221, 44, 0)", "rgb(121, 85, 72)", "rgb(161, 136, 127)", "rgb(141, 110, 99)", "rgb(121, 85, 72)", "rgb(109, 76, 65)", "rgb(93, 64, 55)", "rgb(78, 52, 46)", "rgb(62, 39, 35)", "rgb(158, 158, 158)", "rgb(117, 117, 117)", "rgb(97, 97, 97)", "rgb(66, 66, 66)", "rgb(33, 33, 33)", "rgb(96, 125, 139)", "rgb(120, 144, 156)", "rgb(96, 125, 139)", "rgb(84, 110, 122)", "rgb(69, 90, 100)", "rgb(55, 71, 79)", "rgb(38, 50, 56)", "rgb(0, 0, 0)"]
 let cursor = 0
@@ -28,37 +28,57 @@ class Task {
 }
 class FrameManage {
     constructor() {
+        this.t = 0
         this.tasks = []
-
+        this.nodes = []
+        this.oriCanvas = document.getElementById('nodes')
+        this.canvas = document.getElementById('realCanvas')
+        this.canvas.width = document.body.offsetWidth
+        this.canvas.height = document.body.offsetHeight
+        this.ctx = this.canvas.getContext('2d')
         this.run = this.run.bind(this)
         this.run()
+    }
+    regist(node){
+        this.nodes.push(node)
     }
     add(task) {
         this.tasks.push(task)
     }
-    render() {
+    execute(t, isroll) {
         const task = this.tasks.shift()
         if (!task) {
-            return
+            return false
         }
         for (let i = 0; i < this.tasks.length; i++) {
             if (task.id == this.tasks[i].id && this.tasks[i].t - task.t < 10) {
-                console.log('drop')
-                return
+                return false
             }
         }
         task.func()
+        if(this.tasks[0] && this.tasks[0].t - task.t < t - this.t){
+            this.execute(t, 1)
+        }
+        return true
     }
-    run() {
-        this.render()
+    run(t) {
+        this.execute(t) && this.ctx.drawImage(this.oriCanvas, 0, 0)
         requestAnimationFrame(this.run)
+        this.t = t
     }
 }
 
 class Node extends EventEmitter {
     static container = document.getElementById('nodes');
     static id = 1;
+    static padding = 4;
     static frameManager = null;
+    static setCanvas(){
+        Node.canvas = document.getElementById('nodes')
+        Node.canvas.width = document.body.offsetWidth
+        Node.canvas.height = document.body.offsetHeight
+        Node.ctx = Node.canvas.getContext('2d')
+    }
     constructor(text, isroot) {
         super()
         this.children = []
@@ -66,14 +86,35 @@ class Node extends EventEmitter {
         this.id = Node.id
         Node.id += 1
         this.isroot = !!isroot
-        this.container = Node.container
+        this.canvas = Node.canvas
+        this.ctx = Node.ctx
         this.frameManager = Node.frameManager
-        if (!this.isdata) {
-            this.elem = this.createElement(this.text)
-            this.resize()
-        }
-
+        this.fontSize = 16
+        this.ctx.font = `600 ${this.fontSize}px Arial`
+        this.ctx.fillStyle = "rgba(0, 0, 0, .75)"
+        this.ctx.textAlign = "left"
+        this.height = 16
+        this.left = 0
+        this.top = 0
+        this.size = 10
+        this.width = this.ctx.measureText(this.text).width + Node.padding * 2
         this.applyStyle = this.applyStyle.bind(this)
+    }
+    getEdgeP() {
+        const { left, top, height, width } = this
+        const right = left + width,
+            bottom = top + height
+        const size = this.size
+        if (this.isroot) {
+            return [right - width / 4, top + height / 2 + size / 2]
+        }
+        return [right + 2, bottom + this.size + 1]
+    }
+    getEdgeC() {
+        const { left, top, height } = this
+        const bottom = top + height
+        const size = this.size
+        return [left - 2, bottom + this.size + 1]
     }
     addChild(text) {
         let n
@@ -101,11 +142,11 @@ class Node extends EventEmitter {
         return n
     }
     resize() {
-        const { left, top, width, height } = this.elem.getBoundingClientRect()
-        this.left = left
-        this.top = top
-        this.width = width
-        this.height = height
+        // const { left, top, width, height } = this.elem.getBoundingClientRect()
+        // this.left = left
+        // this.top = top
+        // this.width = width
+        // this.height = height
     }
     moveBy(x, y) {
         this.children.forEach(child => child.moveBy(x, y))
@@ -118,7 +159,6 @@ class Node extends EventEmitter {
     setPos(left, top) {
         this.left = left
         this.top = top
-        this.render()
     }
     createElement(text) {
         let elem = document.createElement('div')
@@ -131,20 +171,81 @@ class Node extends EventEmitter {
         this.container.appendChild(elem)
         return elem
     }
-    applyStyle() {
-        this.elem.style.left = this.left
-        this.elem.style.top = this.top
+    renderArc() {
+        let p0 = this.parent.getEdgeP(),
+            p3 = this.getEdgeC(),
+            p4 = this.getEdgeP(),
+            color = this.color,
+            width = this.size,
+            p1, p2
+        if (p0[0] > p3[0]) {
+            p1 = [p0[0] - 40, p0[1]]
+            p2 = [p3[0] + 70, p3[1]]
+        } else {
+            p1 = [p0[0] + 70, p0[1]]
+            p2 = [p3[0] - 40, p3[1]]
+        }
+        const ctx = this.ctx
+        ctx.beginPath()
+        ctx.lineWidth = width
+        ctx.strokeStyle = color
+        ctx.moveTo.apply(ctx, p0)
+        ctx.bezierCurveTo.apply(ctx, [...p1, ...p2, ...p3])
+        ctx.lineTo.apply(ctx, p4)
+        ctx.stroke()
     }
-    applyStyleDeep() {
-        this.applyStyle()
-        this.children.forEach(child => child.applyStyle())
+    drawBezier({
+        p0,
+        p1,
+        p2,
+        p3,
+        color,
+        width
+    }) {
+        const ctx = this.ctx
+        ctx.beginPath()
+        ctx.lineWidth = width
+        ctx.strokeStyle = color
+        ctx.moveTo.apply(ctx, p0)
+        ctx.bezierCurveTo.apply(ctx, [...p1, ...p2, ...p3])
+        ctx.stroke()
+    }
+    renderLine() {
+        let p0 = this.getEdgeP(),
+            p1 = this.getEdgeC(),
+            color = this.color,
+            width = this.size
+
+        const ctx = this.ctx
+        ctx.beginPath()
+        ctx.moveTo.apply(ctx, p0)
+        ctx.lineWidth = width
+        ctx.strokeStyle = color
+        ctx.lineTo.apply(ctx, p1)
+        ctx.stroke()
+    }
+    applyStyle() {
+        const ctx = this.ctx
+        ctx.font = `600 ${this.fontSize}px Arial`
+        ctx.fillStyle = "rgba(0, 0, 0, .75)"
+        ctx.textAlign = "left"
+        ctx.fillText(this.text, this.left + Node.padding, this.top + this.fontSize)
+        if(this.isroot){
+            return
+        }
+        this.renderArc()
+        this.renderLine()
+    }
+    renderDeep() {
+        this.render()
+        this.children.forEach(child => child.renderDeep())
     }
     render() {
         this.frameManager.add(new Task({
             id: this.id,
             func: this.applyStyle,
             t: Date.now()
-        }))
+        }), this)
     }
     layout() {
         if (!this.children.length) {
@@ -171,21 +272,27 @@ class Node extends EventEmitter {
     }
     static parseNode(data, parent) {
         let n = parent.addChild(data.text)
-        data.children && data.children.forEach(d => {
-            Node.parseNode(d, n)
-        })
-        return n
+        return data.children.length && [n, ...data.children.map(d => (Node.parseNode(d, n)))] || n
     }
     static parse(data) {
         let n = new Node(data.text, true)
-        data.children.map(d => (Node.parseNode(d, n)))
-        return n
+        n.size = 10
+        n.level = 1
+        let nodes = [n]
+        return nodes.concat.apply(nodes, data.children.map(d => (Node.parseNode(d, n))))
     }
 }
 
 var frameManager = new FrameManage()
 Node.frameManager = frameManager
-var rootNode = Node.parse(data)
+Node.setCanvas()
+var nodes = Node.parse(data)
+console.log(nodes)
+nodes.forEach(node => {
+    frameManager.regist(node)
+})
+var rootNode = nodes[0]
 rootNode.layout()
 rootNode.moveBy(500, 400)
 console.log(window.rootNode = rootNode)
+window.rootNode = rootNode
